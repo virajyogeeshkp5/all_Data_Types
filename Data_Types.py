66f57a4d0e16e261c2b36bce9da0e1e3a105ab73
YOGEESH K P <yogeesh793@gmail.com>
2024-08-29 16:40:57 +0530

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,51 @@
+# Data Types
+# int
+print(5156415615+2)
+print(20)
+print(0b10)
+print(0B10)
+print(0x20)
+a=10
+print(a)
+print(type(a))
+b=11
+print(type(b))
+
+# Float
+y=2.35
+print(y)
+print(type(y))
+
+t=35e3
+u=12E4
+o=-35E9
+print(type(t))
+print(type(u))
+print(type(o))
+
+# Bool
+i=True
+l=False
+print(type(i))
+print(type(l))
+
+# String
+y="hi"
+print(y)
+print(type(y))
+h="hi,(')fine"
+print(h)
+print("a \n b")
+print("marchent \n food")
+print("a\tb")
+
+# List
+list1=[1,2,3,'a',5,6,'b',3,[5,6,8,9]]
+print(list1)
+
+x=list()
+print(x)
+
+tuple1 = (1,2,3,4,5)
+j=list(tuple1)
+print(j)
